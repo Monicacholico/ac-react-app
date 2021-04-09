2f42a54604df17d9f2d3d99041496e606337eebd
Monicacholico <monicachol@hotmail>
2021-04-08 22:28:04 -0500

    saving

--- src/api.js
+++ src/api.js
@@ -1,47 +1,47 @@
-// import axios from "axios";
-// import API_KEY from "./credentials";
+import axios from "axios";
+import API_KEY from "./credentials";
 
 
-// // const url='https://cors-anywhere.herokuapp.com/https://sahmed93846.activehosted.com/api/3/contacts';
-// // const config = {
-// //     headers: {
-// //         "Api-Token": `${API_KEY}`,
-// //         "Content-Type": "application/json",
-// //         "Access-Control-Allow-Origin": "*",
-// //         "Access-Control-Allow-Headers": "Content-Type",
-// //         "Access-Control-Allow-Methods": "POST, GET, OPTIONS"
+const url='https://usaa1617144725.activehosted.com/api/3';
+const config = {
+    headers: {
+        "Api-Token": `${API_KEY}`,
+        "Accept": "application/json"
+        // "Access-Control-Allow-Origin": "*",
+        // "Access-Control-Allow-Headers": "Content-Type",
+        // "Access-Control-Allow-Methods": "POST, GET, OPTIONS"
 
-// //     }
+    }
 
-// // }
-// // async function FetchPost() {
-// //     try {
-// //         const response = await axios.get(
-// //             url, config
-// //         );
-// //         console.log(response);
-// //     } catch(error) {
-// //         alert(error.message);
-// //         console.log(error.message);
-// //     }
+}
+async function FetchPost() {
+    try {
+        const response = await axios.get(
+            url, config
+        );
+        console.log(response);
+    } catch(error) {
+        alert(error.message);
+        console.log(error.message);
+    }
 
-// // }
+}
 
-// // FetchPost();
+FetchPost();
 
 
-// export default FetchPost;
+
 
 // function Execute() {
-//   const url = 'https://cors-anywhere.herokuapp.com/https://sahmed93846.activehosted.com/api/3/contacts';
+//   const url = 'https://sahmed93846.activehosted.com/api/3/contacts?status=-1&orders[email]=ASC';
 //   const options = {
-//       method: 'GET',
+//       method: "GET",
 //       headers: {
 //           "Api-Token": `${API_KEY}`,
-//           "Accept": "application/json"
-//           // "Access-Control-Allow-Origin": "*",
-//           // "Access-Control-Allow-Headers": "Content-Type",
-//           // "Access-Control-Allow-Methods": "POST, GET, OPTIONS"
+//           "Content-Type": "application/json",
+//           "Access-Control-Allow-Origin": "*",
+//           "Access-Control-Allow-Headers": "Content-Type",
+//           "Access-Control-Allow-Methods": "POST, GET, OPTIONS"
 //       }
 //   };
 //   return fetch(url, options).then(
@@ -49,21 +49,11 @@
 //           return response.json();
 //       })
 //       .then(data => {
-//           console.log(data);
-//           const listOfContacts = data.contacts;
-//           console.log(listOfContacts);
-//           for(let contact of listOfContacts){
-//             console.log( contact);
-//           }
+//           console.log(data.results);
 //       })
 
 // }
 
-// const listOfContacts = Execute();
-//         console.log(listOfContacts);
+// Execute();
 
-// export default Execute;
-
-
-
-
+export default FetchPost;
--- src/table.js
+++ src/table.js
@@ -1,36 +1,15 @@
 import React, {Component} from 'react';
-import Contact from './Contact';
-import Deal from './Deal';
-import Tag from './Tag';
-import Location from './Location';
-import TotalValue from './TotalValue';
-import './App.css';
+import Execute from './api';
+
 
 
 class Table extends Component {
     render() {
+        const listOfData = Execute();
         return (
-            <section className="section">
-                <table className="legend">
-                    <thead>
-                        <tr className="ac_lists-subheaders">
-                        <th className="table-cell text_left">Contact</th>
-                        <th className="table-cell text_left">Total Value</th>
-                        <th className="table-cell text_left">Deals</th>
-                        <th className="table-cell text_left">Tags</th>
-                        {/* <th className="table-cell text_left">Location</th> */}
-                        </tr>
-                    </thead>
-                <tbody>
-                    <tr className="table-row">
-                    <td className="table-cell name text_underline"><Contact/></td>
-                    <td className=""><TotalValue/></td>
-                    <td className=""><Deal/></td>
-                    <td className=""><Tag/></td>
-                    {/* <td className=""><Location/></td> */}
-                    </tr>
-                </tbody>
-                </table>
+            <section>
+                <h2 className="lavender">Here goes the Table!</h2>
+                {console.log(listOfData)}
             </section>
         )
     }
